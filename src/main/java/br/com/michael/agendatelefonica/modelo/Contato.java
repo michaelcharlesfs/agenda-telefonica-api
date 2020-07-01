d2d66771ae16f7e31a25c41dd6b131e7ab00320d
Michael <michaelcharlesfs@gmail.com>
2020-07-01 15:47:41 -0300

    Adicionar entidade de modelo dos contatos da agenda telefônica

--- src/main/java/br/com/michael/agendatelefonica/modelo/Contato.java
+++ src/main/java/br/com/michael/agendatelefonica/modelo/Contato.java
@@ -1,4 +1,40 @@
 package br.com.michael.agendatelefonica.modelo;
 
+import javax.persistence.*;
+
+@Entity
 public class Contato {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String nome;
+
+    @Column(unique = true, nullable = false)
+    private String telefone;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
 }
