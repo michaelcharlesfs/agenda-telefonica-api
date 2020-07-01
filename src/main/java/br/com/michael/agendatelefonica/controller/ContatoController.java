52e23bfd29f3b5b238eacd91770be4c0d4cb8353
Michael <michaelcharlesfs@gmail.com>
2020-07-01 16:16:28 -0300

    Adicionar paginação padrão para listagem de contatos

--- src/main/java/br/com/michael/agendatelefonica/controller/ContatoController.java
+++ src/main/java/br/com/michael/agendatelefonica/controller/ContatoController.java
@@ -7,6 +7,8 @@ import br.com.michael.agendatelefonica.repository.ContatoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
@@ -22,7 +24,7 @@ public class ContatoController {
     private ContatoRepository contatoRepository;
 
     @GetMapping
-    public ResponseEntity<Page<ContatoDto>> listarContatos(Pageable paginacao) {
+    public ResponseEntity<Page<ContatoDto>> listarContatos(@PageableDefault(page = 0, size = 10, sort = "id", direction = Sort.Direction.ASC) Pageable paginacao) {
         Page<Contato> paginaContatos = contatoRepository.findAll(paginacao);
         return ResponseEntity.ok(ContatoDto.construir(paginaContatos));
     }
